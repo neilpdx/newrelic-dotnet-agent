4bad447e61615c1f0725d5c366f69134eeca96f5
Vu Tran <vuqtran88@gmail.com>
2022-04-12 01:42:55 -0700

    fix target net462

--- build/build_functions.ps1
+++ build/build_functions.ps1
@@ -116,8 +116,8 @@ function Copy-ExtensionsOther {
     Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Core\NewRelic.Agent.Core.Extension\extension.xsd" -Destination "$Destination\extensions" -Force 
 
     if ($Type -like "Framework"){
-        Copy-Item -Path "$RootDirectory\src\_build\AnyCPU-$Configuration\NewRelic.Core\netstandard2.0-ILRepacked\NewRelic.Core.dll" -Destination "$Destination\extensions" -Force 
-        Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Parsing\bin\$Configuration\netstandard2.0\NewRelic.Parsing.dll" -Destination "$Destination\extensions" -Force 
+        Copy-Item -Path "$RootDirectory\src\_build\AnyCPU-$Configuration\NewRelic.Core\net462-ILRepacked\NewRelic.Core.dll" -Destination "$Destination\extensions" -Force 
+        Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Parsing\bin\$Configuration\net462\NewRelic.Parsing.dll" -Destination "$Destination\extensions" -Force 
     }
 
     if ($Type -like "Core"){
@@ -143,8 +143,8 @@ function Copy-AgentRoot {
     Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Core\Config\Configuration.xsd" -Destination "$Destination\newrelic.xsd" -Force 
     
     if ($Type -like "Framework") {
-        Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Extensions\NewRelic.Agent.Extensions\bin\$Configuration\netstandard2.0\NewRelic.Agent.Extensions.dll" -Destination "$Destination" -Force 
-        Copy-Item -Path "$RootDirectory\src\_build\AnyCPU-$Configuration\NewRelic.Agent.Core\netstandard2.0-ILRepacked\NewRelic.Agent.Core.dll" -Destination "$Destination" -Force 
+        Copy-Item -Path "$RootDirectory\src\Agent\NewRelic\Agent\Extensions\NewRelic.Agent.Extensions\bin\$Configuration\net462\NewRelic.Agent.Extensions.dll" -Destination "$Destination" -Force 
+        Copy-Item -Path "$RootDirectory\src\_build\AnyCPU-$Configuration\NewRelic.Agent.Core\net462-ILRepacked\NewRelic.Agent.Core.dll" -Destination "$Destination" -Force 
     }
 
     if ($Type -like "Core") {
